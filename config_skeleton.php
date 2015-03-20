e6503be8cc4246c97c332e4ec7cfdf7d5d882465
Jan Bares <janbares43@gmail.com>
2015-03-20 16:24:42 +0100

    config skeleton

---
+++ config_skeleton.php
@@ -0,0 +1,7 @@
+<?php
+// server url
+define("APP_URL","http://localhost/bt_wireframes/");
+// home path
+define("APP_PATH","V:\\wamp\\www\\bt_wireframes\\");
+// phantomjs path
+define("PHANTOM_PATH","V:\\phantom\\phantomjs");
