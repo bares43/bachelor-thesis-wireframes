57f99837fc9b917b6acde0282eb87e4c7a35c4cb
Jan Bares <janbares43@gmail.com>
2015-03-20 16:55:02 +0100

    config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// openshift
+if(getenv("OPENSHIFT_PHP_IP")){
+    define("APP_URL","http://wireframes-bares43.rhcloud.com/");
+    define("APP_PATH",getenv("HOME")."/app-root/repo/");
+    define("PHANTOM_PATH",getenv("HOME")."/phantomjs/usr/phantomjs --ssl-protocol=any");
+}
+// localhost windows
+else{
+    define("APP_URL","http://localhost/bt_wireframes/");
+    define("APP_PATH","V:\\wamp\\www\\bt_wireframes\\");
+    define("PHANTOM_PATH","V:\\phantom\\phantomjs");
+}
--- config_skeleton.php
+++
@@ -1,7 +0,0 @@
-<?php
-// server url
-define("APP_URL","");
-// home path
-define("APP_PATH","");
-// phantomjs path
-define("PHANTOM_PATH","");
