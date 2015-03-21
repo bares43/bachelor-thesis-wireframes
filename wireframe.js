703478aa420f51066558f3874f081e529465f228
Jan Bares <janbares43@gmail.com>
2015-03-21 10:38:48 +0100

    forms support

--- wireframe.js
+++ wireframe.js
@@ -20,6 +20,34 @@
                 processIframe(jq);
                 walkChilds = false;
             }
+            else if(jq.is("select:isFormSelect")){
+                processFormSelect(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("textarea:isFormTextarea")){
+                processFormTextarea(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("input:isFormRadio")){
+                processFormRadio(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("input:isFormCheckbox")){
+                processFormCheckbox(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("input:isFormFile")){
+                processFormFile(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("input:isFormButton")){
+                processFormButton(jq);
+                walkChilds = false;
+            }
+            else if(jq.is("input:isFormInput")){
+                processFormInput(jq);
+                walkChilds = false;
+            }
             else if(jq.is(":isSlider")){
                 console.log("slider vyhodnoceno "+jq.attr("id"));
                 processSlider(jq);
@@ -31,7 +59,7 @@
                 walkChilds = false;
             }
             // zpracuje jednoravkoe texty
-            else if(jq.is("span:noChild") || jq.is("a:noChild")){
+            else if(jq.is("span:noChild") || jq.is("a:noChild") || jq.is("label")){
                 processOneLineText(jq);
                 walkChilds = true;
             }
@@ -45,6 +73,103 @@
             }
 
 
+        }
+
+        function processFormRadio(radio){
+            var radioWf = $("<input />");
+            radioWf.attr("type","radio");
+            if(radio.is(":checked")){
+                radioWf.attr("checked","checked");
+            }
+
+            radioWf.css("position","absolute");
+            radioWf.css("top",radio.offset().top+"px");
+            radioWf.css("left",radio.offset().left+"px");
+            radioWf.css("width",radio.width()+"px");
+            radioWf.css("height",radio.height()+"px");
+
+            radioWf.appendTo(wireframeContainer);
+        }
+
+        function processFormCheckbox(checkbox){
+            var checkboxWf = $("<input />");
+            checkboxWf.attr("type","checkbox");
+            if(checkbox.is(":checked")){
+                checkboxWf.attr("checked","checked");
+            }
+
+            checkboxWf.css("position","absolute");
+            checkboxWf.css("top",checkbox.offset().top+"px");
+            checkboxWf.css("left",checkbox.offset().left+"px");
+            checkboxWf.css("width",checkbox.width()+"px");
+            checkboxWf.css("height",checkbox.height()+"px");
+
+            checkboxWf.appendTo(wireframeContainer);
+        }
+
+        function processFormFile(file){
+            var fileWf = $("<input />");
+            fileWf.attr("type","file");
+
+            fileWf.css("position","absolute");
+            fileWf.css("top",file.offset().top+"px");
+            fileWf.css("left",file.offset().left+"px");
+            fileWf.css("width",file.width()+"px");
+            fileWf.css("height",file.height()+"px");
+
+            fileWf.appendTo(wireframeContainer);
+        }
+
+        function processFormButton(button){
+            var buttonWf = $("<input />");
+            buttonWf.attr("type","submit");
+
+            buttonWf.attr("value","");
+
+            buttonWf.css("position","absolute");
+            buttonWf.css("top",button.offset().top+"px");
+            buttonWf.css("left",button.offset().left+"px");
+            buttonWf.css("width",button.width()+"px");
+            buttonWf.css("height",button.height()+"px");
+
+            buttonWf.appendTo(wireframeContainer);
+        }
+
+        function processFormInput(input){
+            var inputWf = $("<input />");
+            inputWf.attr("type","text");
+
+            inputWf.css("position","absolute");
+            inputWf.css("top",input.offset().top+"px");
+            inputWf.css("left",input.offset().left+"px");
+            inputWf.css("width",input.width()+"px");
+            inputWf.css("height",input.height()+"px");
+
+            inputWf.appendTo(wireframeContainer);
+        }
+
+        function processFormSelect(select){
+            var selectWf = $("<select />");
+
+            selectWf.css("position","absolute");
+            selectWf.css("top",select.offset().top+"px");
+            selectWf.css("left",select.offset().left+"px");
+            selectWf.css("width",select.width()+"px");
+            selectWf.css("height",select.height()+"px");
+
+            selectWf.appendTo(wireframeContainer);
+        }
+
+        function processFormTextarea(textarea){
+            var textareaWf = $("<textarea />");
+
+            textareaWf.css("position","absolute");
+            textareaWf.css("top",textarea.offset().top+"px");
+            textareaWf.css("left",textarea.offset().left+"px");
+            textareaWf.css("width",textarea.width()+"px");
+            textareaWf.css("height",textarea.height()+"px");
+
+            textareaWf.appendTo(wireframeContainer);
         }
 
         function processIframe(iframe){
@@ -90,9 +215,9 @@
             //imgWF.css("display","block");
             imgWF.css("position","absolute");
 
-            //imgWF.css("background-color","#d7d7d7");
+            imgWF.css("background-color","#d7d7d7");
 
-            imgWF.css("background-image","url('"+img.attr("src")+"')");
+            //imgWF.css("background-image","url('"+img.attr("src")+"')");
 
             imgWF.css("height",img.height()+"px");
             imgWF.css("width",img.width()+"px");
@@ -104,7 +229,7 @@
         }
 
         function processOneLineText(elm){
-            dump(elm);
+            //dump(elm);
 
             var spanWF = $("<div />");
 
@@ -125,10 +250,10 @@
             //console.log(elm.css("font-size"));
 
             //console.log(elm.text().length);
-            //spanWF.lorem({type:"characters",amount:elm.text().length,trim:true});
+            spanWF.lorem({type:"characters",amount:elm.text().length,trim:true});
 
 
-            spanWF.text(elm.text());
+            //spanWF.text(elm.text());
 
             spanWF.css("height",elm.height()+"px");
             spanWF.css("width",elm.width()+"px");
@@ -193,7 +318,27 @@ jQuery.expr[":"].visibleElement = function(elem) {
 jQuery.expr[":"].block = function(elem) {
     return $(elem).css("display") === "block";
 };
-
+jQuery.expr[":"].isFormRadio = function(elem){
+    return (isVisible(elem) && $(elem).is("[type=radio]"));
+};
+jQuery.expr[":"].isFormCheckbox = function(elem){
+    return (isVisible(elem) && $(elem).is("[type=checkbox]"));
+};
+jQuery.expr[":"].isFormFile = function(elem){
+    return (isVisible(elem) && $(elem).is("[type=file]"));
+};
+jQuery.expr[":"].isFormButton = function(elem){
+    return (isVisible(elem) && ($(elem).is("[type=submit]") || $(elem).is("[type=reset]") || $(elem).is("[type=image]") || $(elem).is("button")));
+};
+jQuery.expr[":"].isFormInput = function(elem){
+    return (isVisible(elem) && $(elem).is("input") && !$(elem).is("[type=hidden]") && !$(elem).is(":isFormRadio") && !$(elem).is(":isFormCheckbox") && !$(elem).is(":isFormFile") && !$(elem).is(":isFormButton"));
+};
+jQuery.expr[":"].isFormSelect = function(elem){
+    return (isVisible(elem) && $(elem).is("select"));
+};
+jQuery.expr[":"].isFormTextarea = function(elem){
+    return (isVisible(elem) && $(elem).is("textarea"));
+};
 // try do detect if element is a slider
 jQuery.expr[":"].isSlider = function(elem) {
     elem = $(elem);
