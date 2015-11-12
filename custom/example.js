90b00b40b4b0b52dbe34916c5f94293edda7365c
Jan Bares <janbares43@gmail.com>
2015-11-12 10:53:57 +0100

    custom rules for webpages

---
+++ custom/example.js
@@ -0,0 +1,10 @@
+//WireframeReplacing.before = function(element, response){
+//};
+//WireframeReplacing.after = function(element, response){
+//};
+//WireframeReplacing.processText = function(node, nodeOptions){
+//};
+//WireframeReplacing.processTextBefore = function(){
+//};
+//WireframeReplacing.processTextAfter = function(){
+//};
